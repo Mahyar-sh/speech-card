e7fc93d7acdb73fe44b9e5f740613104f4d857cd
amirmohammad <amir.1774@yahoo.com>
2022-02-06 15:24:51 +0330

    add sample card and items inside tab1

---
+++ src/components/tabs-components/Tab1Decks.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
+
+const Tab1Decks: React.FC = () => {
+ return(
+        <IonCard>
+            <IonItem>hi</IonItem>
+            <IonItem>hi</IonItem>
+            <IonItem>hi</IonItem>
+        </IonCard>
+    );
+};
+ export default Tab1Decks;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,25 @@
+import React from 'react';
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import Tab1Decks from '../components/tabs-components/Tab1Decks';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+        <IonToolbar >
+          <IonTitle>home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+          <IonToolbar style={{borderBottom:' rgb(0,0,0,0.2) 2px solid'}}>
+            <IonTitle size="large">home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-        <IonButton color="primary">Primary</IonButton>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
+        <Tab1Decks/>
       </IonContent>
     </IonPage>
   );
