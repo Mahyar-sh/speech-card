7e29c16a623f1f974616a07180e836081ba785e8
amirmohammad <amir.1774@yahoo.com>
2022-02-07 02:08:04 +0330

    add sample data

---
+++ src/data/firstdata.ts
@@ -0,0 +1,16 @@
+export const firstWords = [
+    {
+        name:'deck 1',
+        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+    },
+    {
+        name:'deck 2',
+        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+    },
+    {
+        name:'deck 3',
+        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+    }
+];
+
+// export default firstWords;
