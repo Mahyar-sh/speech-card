b5e11f6175c9335a411d74e6bdd01c787d302b42
amirmohammad <amir.1774@yahoo.com>
2022-02-08 21:13:38 +0330

    not stable

--- src/components/tabs-components/Tab1Decks.tsx
+++ src/components/tabs-components/Tab1Decks.tsx
@@ -3,37 +3,25 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardH
 import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import HomeDeck from './HomeDeck';
 import { Deck } from '../../models/deck';
+import { IonReactRouter } from '@ionic/react-router';
 
-
-// first words
-const firstWords:Deck[] = [
-    {
-        name:'deck 1',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
-    },
-    {
-        name:'deck 2',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
-    },
-    {
-        name:'deck 3',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
-    }
-  ]; 
 
 type Tab1DecksProps={
     data: Deck[]
 }
 
-const Tab1Decks: React.FC<Tab1DecksProps> = ({data}) => {
+const Tab1Decks: React.FC<Tab1DecksProps> = ({data}):any|undefined => {
     return(
-        <Fragment>
-            {data.map((item,index)=>{
-                return(
-                    <HomeDeck key={index} data={item} index={index} />
-                )
-            })}
-        </Fragment>
+        data.map((item:any|undefined,index:number)=>{
+           item.words.map((word:any)=>{
+               return(
+                <IonCard>
+                    {word.word}<br/>
+                    {word.meanning}
+                </IonCard>
+               )
+           }) 
+        })
     );
 };
  export default Tab1Decks;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+
+import { IonButton, IonContent, IonHeader, IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import Tab1Decks from '../components/tabs-components/Tab1Decks';
 import './Tab1.css';
 
 import {firstWords} from '../data/firstdata';
 import { Deck } from '../models/deck';
+import { IonReactRouter } from '@ionic/react-router';
+
 
 const Tab1: React.FC = () => {
   const [data,setData]= useState<Deck[]>(firstWords);
 
+  const deckBuildRoute = (info: any | undefined):any => {
+    info.map((data:any | undefined, index:number ):any | undefined =>{
+      console.log(data);
+      return(
+        <Route exact path={`deck${index}`}>
+          <Tab1Decks data={data}/>
+        </Route>      );
+    });
+  };
+  const deckBuildButton = (info:any| undefined):any |undefined =>{
+    info.map((data:any | undefined,index:number)=>{
+      return(
+          <IonTabButton tab={`deck${index}`} href={`deck${index}`} >
+            <IonLabel>{info.name}</IonLabel>
+          </IonTabButton>
+      )
+    })
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -23,8 +45,14 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {/* <ExploreContainer name="Tab 1 page" /> */}
-        <Tab1Decks data={data} />
+        <IonReactRouter>
+          <IonRouterOutlet>
+            {deckBuildRoute(data)}
+          </IonRouterOutlet>
+          <IonTabBar slot='top'>
+            {deckBuildButton(data)}  
+            </IonTabBar>            
+        </IonReactRouter>        
       </IonContent>
     </IonPage>
   );
