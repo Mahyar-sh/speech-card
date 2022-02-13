4e345021fbf3ac1a14d09c771d85edcba031a9a8
amirmohammad <amir.1774@yahoo.com>
2022-02-14 01:36:54 +0330

    change data

--- src/data/firstdata.ts
+++ src/data/firstdata.ts
@@ -1,14 +1,14 @@
 export const firstWords = [
     {
-        name:'deck 1',
+        name:'deck1',
         words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
     },
     {
-        name:'deck 2',
+        name:'deck2',
         words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
     },
     {
-        name:'deck 3',
+        name:'deck3',
         words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
     }
 ];
