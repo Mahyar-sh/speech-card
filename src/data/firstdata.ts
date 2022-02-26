e0b800a0dc1d57302e0f4fbd378af4f8cba223f5
amirmohammad <amir.1774@yahoo.com>
2022-02-26 04:06:19 +0330

    change first data

--- src/data/firstdata.ts
+++ src/data/firstdata.ts
@@ -1,15 +1,15 @@
 export const firstWords = [
     {
         name:'deck1',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+        words:[{word:'word 1',meaning:'word 1 meaning'},{word:'word 2', meaning: 'word 2 meaning'},{word: 'word 3',meaning: 'word 3 meaning'}]
     },
     {
         name:'deck2',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+        words:[{word:'word 1',meaning:'word 1 meaning'},{word:'word 2', meaning: 'word 2 meaning'},{word: 'word 3',meaning: 'word 3 meaning'}]
     },
     {
         name:'deck3',
-        words:[{word:'word 1',meanning:'word 1 meanning'},{word:'word 2', meanning: 'word 2 meanning'},{word: 'word 3',meanning: 'word 3 meanning'}]
+        words:[{word:'word 1',meaning:'word 1 meaning'},{word:'word 2', meaning: 'word 2 meaning'},{word: 'word 3',meaning: 'word 3 meaning'}]
     }
 ];
 
